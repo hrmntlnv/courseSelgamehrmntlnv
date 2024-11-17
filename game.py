50c4ea6b0e198c81cf25dc31f50b82e4b5e09f8d
fezeilyt <159615973+fezeilyt@users.noreply.github.com>
2024-11-17 16:13:06 +0100

    новое

--- game.py
+++ game.py
@@ -1,9 +1,8 @@
 import sys 
 import pygame as pg
-import time
-
-
-
+import pygame.mixer as mixer
+mixer.init()
+pg.init()
 
 pg.font.init()
 def_font = pg.font.Font(pg.font.get_default_font(), 30)
@@ -18,8 +17,11 @@ PLATFORM_HEIGHT = 30
 BALL_WIDTH = 50
 BALL_HEIGHT = 50
 FPS = 120
+sound_back_groung = mixer.Sound('back-ground-music.mp3')
+sound_colision = mixer.Sound('Sound.mp3')
+sound_platform = mixer.Sound('Sound-platform.mp3')
 
-def game(screen, clock):
+def game(screen, clock, assets):
 
     
 
@@ -48,10 +50,6 @@ def game(screen, clock):
     BALL_DIRECTION=pg.math.Vector2(0,1).normalize()
 
     COUNTER = 0
-
-
-
-
 
     screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -102,7 +100,7 @@ def game(screen, clock):
     
 
         platform = pg.Rect( PLATFORM_X, PLATFORM_Y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
-        pg.draw.rect( screen, BLACK, platform)
+        # pg.draw.rect( screen, BLACK, platform)
         
         ball = assets['ball'].get_rect()
 
@@ -119,23 +117,23 @@ def game(screen, clock):
 
         # pg.draw.rect( screen, BLACK, ball)
         screen.blit(assets['ball'], ball)
+        screen.blit(assets['panel'], platform)
         text_surface = def_font.render(str(COUNTER), False, (255,0,0))
 
 
 
-
-
-
         if ball.colliderect(platform):
+            sound_platform.play()
             ball_center = (ball.x + ball.width/2, ball.y + ball.height/2)
             platform_center = (platform.x + platform.width/2, platform.y + platform.height/2)
             colision_vector = (ball_center[0]-platform_center[0],ball_center[1]-platform[1])
             BALL_DIRECTION = pg.math.Vector2(colision_vector).normalize()
             COUNTER += 1
-
+        
 
 
         if ball.colliderect(POT_BORDER):
+            sound_colision.play()
             BALL_DIRECTION = BALL_DIRECTION.reflect(pg.math.Vector2(0,1))
 
 
@@ -146,42 +144,35 @@ def game(screen, clock):
 
         # BALL_DIRECTION = BALL_DIRECTION.reflect(pg.math.Vector2(0,-1))
         if ball.colliderect(LEFT_BORDER):
+            sound_colision.play()
             BALL_DIRECTION = BALL_DIRECTION.reflect(pg.math.Vector2(1,0))
 
 
 
         if ball.colliderect(RIGHT_BORDER):
+            sound_colision.play()
             BALL_DIRECTION = BALL_DIRECTION.reflect(pg.math.Vector2(-1,0))
         # if ball.colliderect(pg.Rect(0,0,width=SCREEN_WIDTH,height=0)):
-
-
-
 
 
         screen.blit(text_surface, (20,20))
         pg.display.flip()
         clock.tick(FPS)
 
-
-
-
 if __name__== '__main__':
+    
     screen_out = pg.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-    assets = {
+    assets_int = {
         'ball': pg.transform.scale(pg.image.load('ball.png').convert_alpha(), (BALL_WIDTH, BALL_HEIGHT)),
-        'panel': pg.transform.scale(pg.image.load('panel.png').convert_alpha(), (PLATFORM_WIDTH, PLATFORM_HEIGHT)),
-    }
-
-
+        'panel': pg.transform.scale(pg.image.load('panel.png').convert_alpha(), (PLATFORM_WIDTH, PLATFORM_HEIGHT))
+         }
 
     Clock_out = pg.time.Clock()
 
     while True:
-
-
-
-        game(screen_out,Clock_out)
-
+        sound_back_groung.play()
+        game(screen_out,Clock_out, assets_int)
+        sound_back_groung.stop()
         finish_text = def_font.render('Game Over', False,(0,0,0))
 
         screen_out.blit(finish_text, (SCREEN_WIDTH/2-30, SCREEN_HEIGHT/2))
@@ -197,5 +188,5 @@ if __name__== '__main__':
                 if i.type == pg.QUIT:
                     sys.exit()
             keys = pg.key.get_pressed()
-            if keys [pg.K_r]:
+            if keys [pg.K_0]:
                 break
